d5dc3447f416d9e42a77175d2f67738b96764f8d
Kristofer Svärd <kristofer.svard@gmail.com>
2014-05-14 19:09:28 +0200

    first version

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject gridster-cljs "0.1.0-SNAPSHOT"
+  :description "Clojurescript wrapper for gridster.js"
+  :url "https://github.com/svard/gridster-cljs"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2173"]])
---
+++ src/gridster_cljs/gridster.cljs
@@ -0,0 +1,56 @@
+(ns gridster-cljs.gridster)
+
+(def ^:private $ js/$)
+
+(defn- get-api-object
+  [el]
+  (.data (. ($ el) gridster) "gridster"))
+
+(defn create-grid
+  [grid-el conf]
+  (let [c (clj->js conf)]
+    (.gridster ($ grid-el) c)))
+
+(defn add-widget
+  [grid-el html & [sizex sizey col row]]
+  (let [$grid (get-api-object grid-el)]
+    (-> (.add_widget $grid html sizex sizey col row)
+        (.get))))
+
+(defn resize-widget
+  [grid-el widget & [sizex sizey callback]]
+  (let [$grid (get-api-object grid-el)
+        $widget ($ widget)]
+    (-> (.resize_widget $grid $widget sizex sizey callback)
+        (.get))))
+
+(defn remove-widget
+  [grid-el el & [callback]]
+  (let [$grid (get-api-object grid-el)
+        $el ($ el)]
+    (.remve_widget $grid $el callback)))
+
+(defn serialize
+  [grid-el & [widgets]]
+  (let [$grid (get-api-object grid-el)]
+    (if (nil? widgets)
+      (-> (.serialize $grid)
+          (js->clj :keywordize-keys true))
+      (-> (.serialize $grid ($ widgets))
+          (js->clj :keywordize-keys true)))))
+
+(defn serialize-changed
+  [grid-el]
+  (let [$grid (get-api-object grid-el)]
+    (-> (.serialize_changed $grid)
+        (js->clj :keywordize-keys true))))
+
+(defn enable
+  [grid-el]
+  (let [$grid (get-api-object grid-el)]
+    (.enable $grid)))
+
+(defn disable
+  [grid-el]
+  (let [$grid (get-api-object grid-el)]
+    (.disable $grid)))
